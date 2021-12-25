6052a6722860a0d784f2278665a1a043c9be259f
raflynagachi <rafly.rigannagachi@gmail.com>
2021-12-25 15:51:29 +0700

    create checkout table

---
+++ database/migrations/2021_12_25_084356_create_checkouts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCheckoutsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('checkouts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreignId('camp_id')->constrained();
+            $table->string('card_number', 20);
+            $table->date('expired');
+            $table->string('cvc', 3);
+            $table->boolean('is_paid')->default(false);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('checkouts');
+    }
+}
