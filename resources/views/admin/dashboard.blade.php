77e42321cca30f5c6ce3685970dfe61034ac79dd
raflynagachi <rafly.rigannagachi@gmail.com>
2022-01-02 18:50:29 +0700

    admin set to paid

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container mt-2">
     <div class="row">
-        <div class="col-8 offset-2">
+        <div class="col-10 offset-1">
             <div class="card">
                 <div class="card-header">
                     My Camps
@@ -13,6 +13,7 @@
                     <table class="table">
                         <thead>
                             <tr>
+                                <th>ID</th>
                                 <th>User</th>
                                 <th>Camp</th>
                                 <th>Price</th>
@@ -24,6 +25,7 @@
                         <tbody>
                             @forelse ($checkouts as $checkout)
                                 <tr>
+                                    <td>{{$checkout->id}}</td>
                                     <td>{{$checkout->User->name}}</td>
                                     <td>{{$checkout->Camp->title}}</td>
                                     <td>${{$checkout->Camp->price}},00</td>
@@ -36,10 +38,16 @@
                                         @endif
                                     </td>
                                     <td>
-                                        <form action="" method="post">
+                                        @if (!$checkout->is_paid)
+                                        <form action="{{route('admin.checkout.update', $checkout->id)}}" method="post">
                                             @csrf
-                                            <button class="btn btn-primary btn-sm p-2 py-1">Set to Paid</button>
+                                            <button class="btn btn-primary btn-sm p-2 py-1">
+                                                Set to Paid
+                                            </button>
                                         </form>
+                                        @else
+                                            <button class="btn btn-sm disabled">Already paid</button>
+                                        @endif
                                     </td>
                                 </tr>
                             @empty
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\User\DashboardController as UserDashboard;
 use App\Http\Controllers\Admin\DashboardController as AdminDashboard;
+use App\Http\Controllers\Admin\CheckoutController as AdminCheckout;
 
 /*
 |--------------------------------------------------------------------------
@@ -43,6 +44,7 @@ Route::middleware(['auth'])->group(function () {
     //admin dashboard
     Route::prefix('admin/dashboard')->namespace('Admin')->name('admin.')->middleware('ensureUserRole:admin')->group(function(){
         Route::get('/', [AdminDashboard::class, 'index'])->name('dashboard');
+        Route::post('checkout/{checkout}', [AdminCheckout::class, 'update'])->name('checkout.update');
     });
 
 });
