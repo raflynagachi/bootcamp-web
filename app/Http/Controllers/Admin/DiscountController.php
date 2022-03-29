494f3010eca6e35d864ddd844d46d3819cd6e7ad
raflynagachi <rafly.rigannagachi@gmail.com>
2022-03-29 16:34:21 +0700

    discount store

--- app/Http/Controllers/Admin/DiscountController.php
+++ app/Http/Controllers/Admin/DiscountController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Discount;
 use Illuminate\Http\Request;
+use App\Http\Requests\Admin\Discount\Store;
 
 class DiscountController extends Controller
 {
@@ -34,9 +35,11 @@ class DiscountController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Store $request)
     {
-        //
+        $discount = Discount::create($request->all());
+        $request->session()->flash('success', 'A new discount has been created');
+        return redirect(route('admin.discount.index'));
     }
 
     /**
--- resources/views/admin/discount/create.blade.php
+++ resources/views/admin/discount/create.blade.php
@@ -13,20 +13,37 @@
                         @csrf
                         <div class="form-group mb-4">
                             <label for="" class="form-label">Name</label>
-                            <input type="text" class="form-control">
+                            <input name="name" type="text" class="form-control {{$errors->has('name') ? 'is-invalid' : ''}}" 
+                            value="{{old('name')}}" required>
                         </div>
+                        @if ($errors->has('name'))
+                            <p class="text-danger">{{$errors->first('name')}}</p>
+                        @endif
                         <div class="form-group mb-4">
                             <label for="" class="form-label">Code</label>
-                            <input type="text" class="form-control">
+                            <input name="code" type="text" class="form-control {{$errors->has('code') ? 'is-invalid' : ''}}" 
+                            value="{{old('code')}}" required>
                         </div>
+                        @if ($errors->has('code'))
+                            <p class="text-danger">{{$errors->first('code')}}</p>
+                        @endif
                         <div class="form-group mb-4">
-                            <label for="" class="form-label">Name</label>
-                            <textarea name="description" cols="0" rows="2" class="form-control"></textarea>
+                            <label for="" class="form-label">Description</label>
+                            <textarea name="description" cols="0" rows="2" class="form-control
+                            {{$errors->has('description') ? 'is-invalid' : ''}}" >{{old('description')}}</textarea>
                         </div>
+                        @if ($errors->has('description'))
+                            <p class="text-danger">{{$errors->first('description')}}</p>
+                        @endif
                         <div class="form-group mb-4">
                             <label for="" class="form-label">Discount percentage</label>
-                            <input type="number" class="form-control" min="1" max="100">
+                            <input name="percentage" type="number" class="form-control
+                            {{$errors->has('percentage') ? 'is-invalid' : ''}}" 
+                            value="{{old('percentage')}}" min="1" max="100" required>
                         </div>
+                        @if ($errors->has('percentage'))
+                            <p class="text-danger">{{$errors->first('percentage')}}</p>
+                        @endif
                         <div class="form-group mb-4">
                             <button class="btn-primary">Submit</button>
                         </div>
